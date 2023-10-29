68235686ac6b57d05942ead7756b363669704228
jeffersonrolino <jefferson.rolino@gmail.com>
2023-10-28 22:07:56 -0300

    Render data (cart.html)

--- store/views.py
+++ store/views.py
@@ -10,7 +10,6 @@ def store(request):
 
 
 def cart(request):
-    
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
